178a1147714fcc22415b9c5f99d4aece89a81dc9
nazzoy <nazoyheh@gmail.com>
2026-04-28 19:14:41 +0500

    homework_5 - Новиков Антон

--- homeworks/homework_5/src/main/scala/Task2.scala
+++ homeworks/homework_5/src/main/scala/Task2.scala
@@ -11,16 +11,52 @@ import cats.implicits._
 object Task2 extends App {
   case class RadiusVector(x: Int, y: Int)
   object RadiusVector {
-    implicit val monoid: Monoid[RadiusVector] = ???
+    implicit val monoid: Monoid[RadiusVector] = new Monoid[RadiusVector] {
+      def empty: RadiusVector = RadiusVector(0, 0)
+      def combine(a: RadiusVector, b: RadiusVector): RadiusVector =
+        RadiusVector(a.x + b.x, a.y + b.y)
+    }
   }
   case class DegreeAngle(angel: Double)
   object DegreeAngle {
-    implicit val monoid: Monoid[DegreeAngle] = ???
+    def apply(degrees: Double): DegreeAngle = new DegreeAngle(normalize(degrees))
+
+    private def normalize(degrees: Double): Double = {
+      val mod = degrees % 360.0
+      if (mod < 0) mod + 360.0 else mod
+    }
+
+    implicit val monoid: Monoid[DegreeAngle] = new Monoid[DegreeAngle] {
+      def empty: DegreeAngle = DegreeAngle(0.0)
+      def combine(a: DegreeAngle, b: DegreeAngle): DegreeAngle =
+        DegreeAngle(a.angel + b.angel)
+    }
   }
 
   case class SquareMatrix[A : Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
   object SquareMatrix {
-    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = ???
+    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = new Monoid[SquareMatrix[A]] {
+      private val monoidA = implicitly[Monoid[A]]
+
+      def empty: SquareMatrix[A] = {
+        val e = monoidA.empty
+        SquareMatrix(((e, e, e), (e, e, e), (e, e, e)))
+      }
+
+      def combine(a: SquareMatrix[A], b: SquareMatrix[A]): SquareMatrix[A] = {
+        def combineTuples(t1: (A, A, A), t2: (A, A, A)): (A, A, A) = (
+          monoidA.combine(t1._1, t2._1),
+          monoidA.combine(t1._2, t2._2),
+          monoidA.combine(t1._3, t2._3)
+        )
+
+        SquareMatrix((
+          combineTuples(a.values._1, b.values._1),
+          combineTuples(a.values._2, b.values._2),
+          combineTuples(a.values._3, b.values._3)
+        ))
+      }
+    }
   }
 
   val radiusVectors = Vector(RadiusVector(0, 0), RadiusVector(0, 1), RadiusVector(-1, 1))
